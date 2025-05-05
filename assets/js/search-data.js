5c1a9aefeb7aa027f2a71c554ad66767d0a72d38
0zzung0 <qnr1502@snu.ac.kr>
2025-05-05 05:54:51 +0000

    Deploying to gh-pages from @ 0zzung0/0zzung0.github.io@10710226b3681a8dac690017ab29f2f8647da9d6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -83,6 +83,17 @@ ninja.data = [{
             window.location.href = "/blog/2025/markdown/";
           
         },
+      },{id: "post-취미-소개",
+        
+          title: "취미 소개",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/hobby/";
+          
+        },
       },{id: "post-나의-첫-jekyll-블로그-포스트",
         
           title: "나의 첫 Jekyll 블로그 포스트",
